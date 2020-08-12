d8104362f3d37a01b23e2803053be85d21ea3d98
Kim Pepper <kim@previousnext.com.au>
2020-08-12 17:50:43 +1000

    Fixes linting and adds circleci builds

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,9 +1,11 @@
-import {skprConfig} from "../index"
+import skprConfig from "../src/index"
 
 test('loads config', () => {
     let config = skprConfig(__dirname + '/fixtures/config.json')
     expect(config.foo).toBe("bar")
     expect(config.wiz).toBe("bang")
+    expect(config['pop-fiz']).toBe("whoop")
     expect(process.env['FOO']).toBe("bar")
     expect(process.env['WIZ']).toBe("bang")
+    expect(process.env['POP_FIZ']).toBe("whoop")
 });
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+
+// Converts a key name into a suitable environment variable name.
+function convertToEnvVarName(key) {
+  return key.replace(/[^A-Za-z0-9 ]/g, '_').toUpperCase();
+}
+
+// Loads the skpr config into and object.
+function load(path = '/etc/skpr/data/config.json') {
+  const data = fs.readFileSync(path, 'utf-8');
+  const config = JSON.parse(data);
+  Object.keys(config).forEach((key) => {
+    process.env[convertToEnvVarName(key)] = config[key];
+  });
+  return config;
+}
+
+export default load;
