d312cc761e7dba820ffff4dc69c255bb646bdc47
Kim Pepper <kim@previousnext.com.au>
2020-08-12 18:55:55 +1000

    Feedback from larowlan

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import fs from 'fs';
 
 // Converts a key name into a suitable environment variable name.
-function convertToEnvVarName(key) {
+const convertToEnvVarName = (key) => {
   return key.replace(/[^A-Za-z0-9 ]/g, '_').toUpperCase();
-}
+};
 
 // Loads the skpr config into an object.
-function load(path = '/etc/skpr/data/config.json') {
+const skprConfig = (path = '/etc/skpr/data/config.json') => {
   const data = fs.readFileSync(path, 'utf-8');
   const config = JSON.parse(data);
   Object.keys(config).forEach((key) => {
     process.env[convertToEnvVarName(key)] = config[key];
   });
   return config;
-}
+};
 
-export default load;
+export default skprConfig;
